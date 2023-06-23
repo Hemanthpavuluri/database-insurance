3dd0579e2ab0b2fee08cd08c541498a1dccc3bfd
Hemanthpavuluri <68024503+Hemanthpavuluri@users.noreply.github.com>
2023-06-23 15:10:21 -0500

    Add files via upload

---
+++ proj2-makeSchema-A05138724.sql
@@ -0,0 +1,230 @@
+--student id:A05138724 , HEMANTH PAVULURI
+set termout on
+prompt Building EX Insurance Company database. Please wait...
+set pagesize 50
+
+drop TABLE Address CASCADE CONSTRAINT PURGE;
+drop TABLE Company CASCADE CONSTRAINT PURGE;
+drop TABLE Employee CASCADE CONSTRAINT PURGE;
+drop TABLE Customer CASCADE CONSTRAINT PURGE;
+drop TABLE Policy CASCADE CONSTRAINT PURGE;
+drop TABLE Payment CASCADE CONSTRAINT PURGE;
+drop TABLE Claim CASCADE CONSTRAINT PURGE;
+drop TABLE Claimant CASCADE CONSTRAINT PURGE;
+drop TABLE Incident CASCADE CONSTRAINT PURGE;
+drop TABLE Property CASCADE CONSTRAINT PURGE;
+drop TABLE Payment_plan CASCADE CONSTRAINT PURGE;
+drop TABLE Reserve CASCADE CONSTRAINT PURGE;
+
+
+
+CREATE TABLE Address(address_id number(5) PRIMARY KEY,
+	street_name varchar(45) ,
+	state varchar(25) ,
+	city varchar(20) ,
+	zip_code number(5));
+
+INSERT INTO Address values(23281,'1834 Post Road','Texas','San Marcos',78666);
+INSERT INTO Address values(23282,'6 Pines Rd','Texas','San Marcos',78555);
+INSERT INTO Address values(23283,'Acorn St','Texas','San Marcos',76663);
+INSERT INTO Address values(23284,'Barnes Dr','Texas','San Marcos',71222);
+INSERT INTO Address values(23285,'Buttercup St','Texas','San Marcos',73232);
+INSERT INTO Address values(23286,'Canyon Rd','Texas','San Marcos',74324);
+INSERT INTO Address values(23287,'Columbia St','Texas','Austin',54423);
+INSERT INTO Address values(23288,'Comal St','Texas','Dallas',98989);
+INSERT INTO Address values(23289,'County Rd','Texas','San Antonio',12132);
+INSERT INTO Address values(23290,'Daisy St','Oregon','Portland',98773);
+INSERT INTO Address values(23291,'Encino St','Washington','Seattle',13444);
+INSERT INTO Address values(24532,'Cesar Chavez St','Austin','Texas',76542);
+INSERT INTO Address values(25433,'Cedar Park','Austin','Texas',65554);
+INSERT INTO Address values(25552,'Leander','Austin','Texas',78433);
+INSERT INTO Address values(27622,'Churchill St','San Marcos','Texas',98822);
+
+CREATE TABLE Company(name varchar(15) PRIMARY KEY,
+	cphone number(10) ,
+	no_of_branches number(5),
+	no_of_employees number(7),
+	address_id number(5) REFERENCES Address(address_id) ON DELETE CASCADE);
+
+INSERT INTO Company values('EX Insurance',9887732334,100,5000,23281);
+
+
+CREATE TABLE Employee(emp_id number(4) PRIMARY KEY,
+	first_name varchar(35) ,
+	last_name varchar(35) ,
+	phone_emp number(10) ,
+	type_of_employee varchar(20) ,
+	address_id number(5) REFERENCES Address(address_id) ON q DELETE CASCADE,
+	name varchar(15) REFERENCES Company(name) ON DELETE CASCADE);
+
+INSERT INTO Employee values(1221,'Adam','Abhram',5123432222,'Agent',23282,'EX Insurance');
+INSERT INTO Employee values(1222,'Adrian','Alen',5124346778,'Rater',23283,'EX Insurance');
+INSERT INTO Employee values(1223,'Blake','Ball',5122323322,'Agent',23284,'EX Insurance');
+INSERT INTO Employee values(1224,'Boris','Bell',7379933322,'Underwriter',23285,'EX Insurance');
+INSERT INTO Employee values(1225,'Michael','James',2324433327,'Adujuster',23286,'EX Insurance');
+INSERT INTO Employee values(1226,'Peter','Brading',7376885513,'Rater',23291,'EX Insurance');
+INSERT INTO Employee values(1227,'Pete','Cheverton',8943422211,'Agent',24532,'EX Insurance');
+
+
+CREATE TABLE Customer(customer_id number(5) PRIMARY KEY,
+	first_name varchar(20),
+	last_name varchar(20),
+	phone_c number(10),
+	c_type varchar(45),
+	email varchar(30),
+	dob DATE,
+	age number(3),
+	claim_id number(5),
+	property_id number(5),
+	policy_number number(10),
+	address_id number(5) REFERENCES Address(address_id) ON DELETE CASCADE);
+
+
+INSERT INTO Customer values(33223,'Ivor','Thompson',5129093322,'Individual','ivorthompson@gmail.com',TO_DATE('10/05/1998','mm/dd/yyyy'),23,23421,95950,6565532232,23287);
+INSERT INTO Customer values(33443,'Harry','Brown',5135543211,'Individual','harryb@gmail.com',TO_DATE('01/01/1993','mm/dd/yyyy'),28,'',95951,7565655231,23288);
+INSERT INTO Customer values(44222,'Benjamin','Guy',4324441199,'Individual','benjamin123@gmail.com',TO_DATE('02/23/1992','mm/dd/yyyy'),29,'',95952,1341214324,23289);
+INSERT INTO Customer values(22445,'Laurence','Hendy',9874332111,'Individual','laurence33@gmail.com',TO_DATE('05/20/2000','mm/dd/yyyy'),21,98231,95953,9409394328,23290);
+INSERT INTO Customer values(66553,'Jhon','Doe',4324551433,'Corporate','jhondoe@gmail.com',TO_DATE('07/19/1990','mm/dd/yyyy'),29,22332,95954,4353254322,23291);
+INSERT INTO Customer values(23413,'Roland','Cotter',7379989313,'Individual','roland@gmail.com',TO_DATE('05/11/1994','mm/dd/yyyy'),27,76643,95344,2345254422,25552);
+INSERT INTO Customer values(18721,'Henry','Russel',8981313331,'Individual','henry22@gmail.com',TO_DATE('02/14/1996','mm/dd/yyyy'),25,98212,95411,2653232988,27622);
+
+
+CREATE TABLE Policy(policy_number number(10) PRIMARY KEY,
+	deductible number(10),
+	issue_date DATE,
+	type varchar(30),
+	coverages varchar(500),
+	coverage_period varchar(45),
+	premium float(10),
+	payment_id number(5),
+	customer_id number(10) REFERENCES Customer(customer_id) ON DELETE CASCADE,
+	emp_id number(4) REFERENCES Employee(emp_id) ON DELETE CASCADE,
+	name varchar(15) REFERENCES Company(name) ON DELETE CASCADE);
+
+INSERT INTO Policy values(6565532232,5000,TO_DATE('03/03/2020','mm/dd/yyyy'),'home','personal property,electronics,personal liability,loss of use','03/03/2020 - 03/04/2021',700.00,29043,33223,1221,'EX Insurance');
+INSERT INTO Policy values(7565655231,6000,TO_DATE('05/30/2020','mm/dd/yyyy'),'automobile','collosion,body injury,property damage,emergency road service','05/30/2020 - 05/31/2021',1000.00,29044,33443,1222,'EX Insurance');
+INSERT INTO Policy values(1341214324,5000,TO_DATE('02/22/2021','mm/dd/yyyy'),'automobile','collosion,body injury,property damage,emergency road service','02/22/2021 - 02/23/2022',849.99,28223,44222,1223,'EX Insurance');
+INSERT INTO Policy values(9409394328,5000,TO_DATE('04/12/2020','mm/dd/yyyy'),'home','personal property,electronics,personal liability,loss of use','04/12/2020 - 04/13/2021',500.00,20102,22445,1224,'EX Insurance');
+INSERT INTO Policy values(4353254322,5000,TO_DATE('09/23/2021','mm/dd/yyyy'),'home','personal property,electronics,personal liability,loss of use','09/23/2021 - 09/24/2022',982.55,13232,66553,1225,'EX Insurance');
+INSERT INTO Policy values(2345254422,9000,TO_DATE('01/01/2021','mm/dd/yyyy'),'home','personal property,electronics,personal liability,loss of use','01/01/2021 - 01/02/2022',400.20,15432,23413,1226,'EX Insurance');
+INSERT INTO Policy values(2653232988,10000,TO_DATE('06/02/2021','mm/dd/yyyy'),'automobile','collosion,body injury,property damage,emergency road service,loss of use','06/02/2021 - 06/03/2022',784.45,16532,18721,1227,'EX Insurance');
+
+
+CREATE TABLE Payment(payment_id number(5) PRIMARY KEY,
+	payment_mode varchar(15),
+	status varchar(10),
+	payment_amt float(10),
+	payment_date DATE,
+	emp_id number(4) REFERENCES Employee(emp_id) ON DELETE CASCADE,
+	customer_id number(10) REFERENCES Customer(customer_id) ON DELETE CASCADE,
+	policy_number number(10) REFERENCES Policy(policy_number) ON DELETE CASCADE);
+
+INSERT INTO	Payment values(29043,'online','success',700.00,TO_DATE('03/03/2020','mm/dd/yyyy'),1221,33223,6565532232);
+INSERT INTO	Payment values(29044,'cheque','success',1000.00,TO_DATE('05/30/2020','mm/dd/yyyy'),1222,33443,1341214324);
+INSERT INTO	Payment values(28223,'bank transfer','success',849.99,TO_DATE('02/22/2021','mm/dd/yyyy'),1224,22445,9409394328);
+INSERT INTO	Payment values(20102,'online','success',500.00,TO_DATE('09/23/2021','mm/dd/yyyy'),1223,66553,4353254322);
+INSERT INTO	Payment values(13232,'cheque','success',982.55,TO_DATE('12/01/2018','mm/dd/yyyy'),1225,44222,7565655231);
+INSERT INTO Payment values(15432,'online','success',400.20,TO_DATE('01/01/2021','mm/dd/yyyy'),1226,23413,2345254422);
+INSERT INTO Payment values(16532,'online','success',784.45,TO_DATE('06/02/2021','mm/dd/yyyy'),1227,18721,2653232988);
+
+
+CREATE TABLE Claim(claim_id number(5) PRIMARY KEY,
+	status varchar(20),
+	amount number(10),
+	payment_date DATE, 
+	policy_number number(10) REFERENCES Policy(policy_number) ON DELETE CASCADE,
+	payment_id number(5) REFERENCES Payment(payment_id) ON DELETE CASCADE,
+	emp_id number(4) REFERENCES Employee(emp_id) ON DELETE CASCADE,
+	customer_id number(10) REFERENCES Customer(customer_id) ON DELETE CASCADE);
+
+INSERT INTO Claim values(23421,'settled',10000,TO_DATE('02/22/2021','mm/dd/yyyy'),6565532232,29043,1221,33223);
+INSERT INTO Claim values(76643,'settled',10000,TO_DATE('12/23/2021','mm/dd/yyyy'),2345254422,15432,1226,23413);
+INSERT INTO Claim values(98212,'settled',20000,TO_DATE('12/10/2021','mm/dd/yyyy'),2653232988,16532,1227,18721);
+INSERT INTO Claim values(98231,'processing',20000,TO_DATE('04/10/2021','mm/dd/yyyy'),9409394328,29044,1224,22445);
+INSERT INTO Claim values(22332,'settled',25000,TO_DATE('01/13/2021','mm/dd/yyyy'),4353254322,28223,1225,66553);
+
+
+CREATE TABLE Claimant(claimant_id number(6) PRIMARY KEY,
+	clname varchar(15),
+	phone_num number(10),
+	claim_id number(5) REFERENCES Claim(claim_id) ON DELETE CASCADE);
+
+INSERT INTO Claimant values(438892,'Baker',7278833212,23421);
+INSERT INTO Claimant values(438893,'Adams',8383223311,76643);
+INSERT INTO Claimant values(438894,'Lopez',9832213313,98212);
+INSERT INTO Claimant values(438895,'Jones',3432248811,98231);
+INSERT INTO Claimant values(438896,'Smith',8743444253,22332);
+
+CREATE TABLE Incident(report_id number(6) PRIMARY KEY,
+	inc_date DATE,
+	type varchar(30));
+
+INSERT INTO	Incident values(322214,TO_DATE('02/23/2018','mm/dd/yyyy'),'Car accident');
+INSERT INTO Incident values(323222,TO_DATE('03/14/2019','mm/dd/yyyy'),'Flood damage');
+INSERT INTO	Incident values(543211,TO_DATE('05/10/2018','mm/dd/yyyy'),'Hurrican damage');
+INSERT INTO	Incident values(987653,TO_DATE('09/12/2018','mm/dd/yyyy'),'Truck accident');
+INSERT INTO Incident values(884332,TO_DATE('10/09/2021','mm/dd/yyyy'),'Car accident');
+
+
+CREATE TABLE Property(property_id number(5) PRIMARY KEY,
+	owner_name varchar(20),
+	type_of_property varchar(30),
+	accident varchar(4),
+	report_id number(6) REFERENCES Incident(report_id) ON DELETE CASCADE,
+	address_id number(5) REFERENCES Address(address_id) ON DELETE CASCADE,
+	customer_id number(10) REFERENCES Customer(customer_id) ON DELETE CASCADE);
+
+INSERT INTO Property values(95950,'Ivor','house',1,323222,23287,33223);
+INSERT INTO Property values(95951,'Benjamin','house',1,543211,23289,44222);
+INSERT INTO Property values(95952,'Laurence','automobile',1,322214,23290,22445);
+INSERT INTO Property values(95953,'Jhon','house',0,'',23291,66553);
+INSERT INTO Property values(95954,'Harry','automobile',0,'',23288,33443);
+INSERT INTO Property values(95344,'Roland','Cotter',1,987653,25552,23413);
+INSERT INTO Property values(95411,'Henry','Russel',1,884332,27622,18721);
+
+
+CREATE TABLE Payment_plan(p_id number(6) PRIMARY KEY,
+	ptype varchar(30),
+	payment_id number(5) REFERENCES Payment(payment_id) ON DELETE CASCADE);
+
+INSERT INTO Payment_plan values(432552,'online',29043);
+INSERT INTO Payment_plan values(421877,'cheque',29044);
+INSERT INTO Payment_plan values(414122,'bank transfer',28223);
+INSERT INTO Payment_plan values(409212,'online',20102);
+INSERT INTO Payment_plan values(487212,'cheque',13232);
+
+
+
+CREATE TABLE Reserve(r_id number(6) PRIMARY KEY,
+	reserve_amt number(10),
+	claim_id number(5) REFERENCES Claim(claim_id) ON DELETE CASCADE,
+	emp_id number(4) REFERENCES Employee(emp_id) ON DELETE CASCADE);
+
+INSERT INTO Reserve values(986543,20000,23421,1221);
+INSERT INTO Reserve values(865622,20000,76643,1226);
+INSERT INTO Reserve values(134322,20000,98212,1227);
+INSERT INTO Reserve values(987221,30000,98231,1224);
+INSERT INTO Reserve values(873422,30000,22332,1225);
+
+
+ALTER TABLE Customer ADD CONSTRAINT claim_id FOREIGN KEY(claim_id) REFERENCES Claim(claim_id) ON DELETE CASCADE;
+ALTER TABLE Customer ADD CONSTRAINT property_id FOREIGN KEY(property_id) REFERENCES Property(property_id) ON DELETE CASCADE;
+ALTER TABLE Customer ADD CONSTRAINT policy_number FOREIGN KEY(policy_number) REFERENCES Policy(policy_number) ON DELETE CASCADE;
+ALTER TABLE Policy ADD CONSTRAINT payment_id FOREIGN KEY(payment_id) REFERENCES Payment(payment_id) ON DELETE CASCADE;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ project3.sql
@@ -0,0 +1,232 @@
+-- Cs5332 Project, Stage 3
+-- ### HEMANTH PAVULURI -- A05138724 ###
+set pagesize 300;
+set linesize 300;
+
+column name format a20;
+column salary format $9999990.00;
+create or replace view q1 as
+	select party.givenname|| ' '|| party.familyname as NAME, employee.salary as SALARY from party 
+        inner join employee on party.id=employee.id and employee.position='Underwriter';
+
+create or replace view q2 as
+	select givenname|| ' ' || familyname as NAME, street||', '||suburb || ' '  ||state|| ' ' ||POSTCODE  as address from party where id IN
+        (select client from holds where policy IN(select policy from covers where coverage in
+        (select coverage from ratingaction where action='D'))) order by name;
+
+column name format a20;
+create or replace view q3 as
+	select givenname|| ' ' || familyname as name from party where id in 
+        (select id from employee where id in(select client from policy inner join holds on policy.id=holds.policy and policy.status='OK'));
+
+column MoneyCollected format $9999990.00
+column MoneyCollected heading "Total|Money|Collected"
+create or replace view q4 as
+	select sum(premium) as MoneyCollected from policy;
+
+
+column MoneyPaid format $9999990.00
+column MoneyPaid heading "Total|Money|Paid Out"
+create or replace view q5 as
+	select sum(amount) as MoneyPaid from claimaction where action='PO';
+
+column name format a20
+create or replace view q6 as
+	select givenname|| ' '||familyname as NAME from party where id in
+        (select claim.claimant from claim inner join holds on claim.claimant=holds.client and claim.policy=holds.policy);
+
+create or replace view q7 as
+	select distinct(policy) from coverage inner join covers on coverage.id=covers.coverage 
+        inner join covereditem on covers.item=covereditem.id where covervalue > marketvalue order by policy;
+
+column make format a12
+column model format a12
+column NumberInsured format 999 heading "#"
+create or replace view q8 as
+	select make,model,count(make) as NumberInsured from covereditem group by make,model order by make;
+
+column PolicyHolder format a20
+column PolicyHolder heading "Employee|Holding|Policy"
+column PolicyProcessor format a20
+column PolicyProcessor heading "Employee|Processing|Policy"
+create or replace view q9 as
+	select prt1.givenname||' '||prt1.familyname as PolicyHolder, prt2.givenname||' '||prt2.familyname as PolicyProcessor from party prt1, party prt2, holds hld,employee emp  where 
+        prt1.id= hld.client and emp.id=hld.client and emp.id=prt1.id and prt2.id IN 
+        ((select ra.rater from ratingaction ra,covers cv,holds hl where ra.coverage=cv.coverage and hl.policy=cv.policy and hl.client=prt1.id ) UNION 
+        (select uw.underwriter from underwritingaction uw,holds where uw.policy=holds.policy and holds.client=prt1.id))  order by PolicyHolder, PolicyProcessor;
+
+column id heading "Policy#"
+column make heading "Make"
+column model heading "Model"
+create or replace view q10 as
+	select distinct b.policy as id,a.make,a.model from covereditem a inner join covers b on b.item=a.id where not exists
+        ((select distinct(description) from coverage) MINUS 
+        (select description from coverage inner join covers on covers.coverage=coverage.id where covers.policy=b.policy));
+
+
+--PL/SQL;
+create or replace
+procedure discount
+is
+cursor d is select givenname|| ' '||familyname as name,policy,premium from holds 
+inner join policy on holds.policy=policy.id inner join party on party.id=holds.client where client in
+(select id from employee) and premium is not null;
+begin 
+--displaying data before update.
+dbms_output.put_line('Displaying the data before update:');
+dbms_output.put_line('---------------');
+FOR cd in d LOOP
+        dbms_output.put_line('NAME'||':'||cd.name);
+        dbms_output.put_line('Policy'||':'||cd.policy);
+        dbms_output.put_line('Premium'||':'||TO_CHAR(cd.premium,'$9999990.00'));
+        dbms_output.put_line('---------------');
+END LOOP;
+
+--update query
+        update policy set premium=premium-(premium*10)/100 where id in
+        (select distinct h.policy from holds h,policy p,covers c, employee e where h.policy=c.policy and p.id=h.policy and e.id=h.client and p.status='OK');
+dbms_output.put_line('Displaying data after update:');
+
+--displaying data after update.
+dbms_output.put_line('---------------');
+FOR j in(select givenname|| ' '||familyname as name,policy,premium from holds inner join policy on holds.policy=policy.id inner join party on party.id=holds.client where client in(select id from employee) and premium is not null) LOOP
+        dbms_output.put_line('NAME'||':'||j.name);
+        dbms_output.put_line('Policy'||':'||j.policy);
+        dbms_output.put_line('Premium'||':'||TO_CHAR(j.premium,'$9999990.00'));
+        dbms_output.put_line('---------------');
+END LOOP;
+end;
+/
+
+
+CREATE OR REPLACE PROCEDURE claims(policyID integer)
+IS
+totalclaims number:=0;  -- total count of claims
+lodgeby varchar(40);  -- name of the person who lodge the claim
+actor varchar(40);    --name of the person the money paid to
+handler varchar(40);  --name of the person the money paid by
+opener varchar(40);   --name of the person who opened the claim
+closer varchar(40);  -- name of the person who closed the claim
+cpol integer:=0;    -- count of the policies.
+logclose  claimaction.happened%TYPE;  -- date of the claim happened.
+openamt claimaction.amount%TYPE;   --total amount of the claim
+i number:=1;
+--cursor cid1 to fetch all the claims for the given policy.
+CURSOR cid1 is select * from claim where policy=policyID order BY id desc;
+BEGIN
+--count to check whether the policy is present in database or not.
+select count(id) into cpol from policy where id=policyID;
+if(cpol = 0) then
+dbms_output.put_line('There is no policy: '||policyID);
+ELSE
+--fetching total count of claims.
+select count(id) into totalclaims from claim where policy=policyID;
+dbms_output.put_line('Policy'||' '|| policyID||' '|| 'has'||' '|| totalclaims ||' '|| 'claims.');
+dbms_output.put_line('---------------');
+if(totalclaims > 0) then
+        --looping the cursor
+        FOR c in cid1 LOOP
+                dbms_output.put_line(i||'.'||' '||'Claim'|| ' '||c.id);
+
+                --fetching the name of the person who lodges the claim.
+                select givenname|| ' '|| familyname as name into lodgeby from party where id=c.claimant;
+                --fetching the total amount of the claim.
+                select ca.amount into openamt from claimaction ca where ca.claim=c.ID and ca.action='OP';
+
+                dbms_output.put_line('Lodged by :'||' '|| lodgeby||' '||'on'||' '||c.lodgedate);
+                dbms_output.put_line('Event Date :'|| ' '||c.eventdate);
+                dbms_output.put_line('Reserve at:     '||TO_CHAR(c.reserve,'$9999990.00'));
+
+                if(c.status='A') then
+                        dbms_output.put_line('Status    : open');
+                else
+                        --fetch the name of the person who closed the claim.
+                        select givenname|| ' '|| familyname as name into closer from party where id in
+                        (select handler from claimaction ca where ca.claim=c.id and ca.action='CL');
+                        dbms_output.put_line('Status    : closed');
+                END IF;
+
+                dbms_output.put_line('Activity  :');
+                
+                --query to fetch the name of the person who opens the claim.
+                select givenname|| ' '|| familyname as name into opener from party where id in
+                (select handler from claimaction ca where ca.claim=c.id and ca.action='OP');
+                dbms_output.put_line('...Claim opened by '|| opener || ' '||'on'||' '|| c.lodgedate||'with reserve set to     '||' '||TO_CHAR(openamt,'$9999990.00')||'.');
+                
+                --looping the rows for the claims that are paid out.
+                FOR k in (select * from claimaction ca where ca.claim=c.id and ca.action='PO') LOOP
+                        select givenname|| ' '|| familyname as name into actor from party where id=k.actor;
+                        select givenname|| ' '|| familyname as name into handler from party where id=k.handler;
+                        dbms_output.put_line('...    '||TO_CHAR(k.amount,'$9999990.00')||' '||'paid out to '|| actor||' '||'by '||handler||' on '||k.happened||'.');
+                END LOOP;
+                
+                --check if claim closed or not.
+                if(c.status='Z') then
+                        --fetching the claim happened date.
+                        select ca.happened into logclose from claimaction ca where ca.action='CL' and ca.claim=c.id;
+                        dbms_output.put_line('...Claim closed by '|| closer || ' '||'on'||' '|| logclose||'.');
+                END IF;
+                i:=i+1;
+                dbms_output.put_line('---------------');
+        END LOOP;
+END IF;
+END IF;
+END;
+/
+
+
+create or replace procedure policy_rework_list
+is
+i integer:=1;
+rcheck integer:=0; --count of ratingaction policy declined.
+ucheck integer:=0; --count of underwritingaction policy declined.
+totalrework integer:=0; --count of policies that need to re-worked.
+rerated integer:=0;  
+reunderwrited integer:=0; 
+
+--cursor rdt to fetch the policies in ratingaction that are declined by rater.
+CURSOR rdt is select distinct cv.policy from covers cv,ratingaction ra where 
+cv.coverage=ra.coverage and ra.action='D';
+--cursor ct to fetch the policies in underwrintingaction that are declined by underwriter.
+CURSOR ct is select distinct uw.policy from underwritingaction uw where uw.action='D';
+
+BEGIN
+--query to get the total number of rows that are declined by the rater or underwriter.
+select count(distinct cv.policy) into totalrework from covers cv,ratingaction ra,underwritingaction uw where 
+cv.coverage=ra.coverage and ra.action='D' or uw.action='D';
+--query to get the total number of rows thata are declined by the underwriter.
+select count(distinct uw.policy) into ucheck from underwritingaction uw where uw.action='D';
+dbms_output.put_line('Total number of policies that need to be re-worked are : '||totalrework);
+if(totalrework>0) then
+        dbms_output.put_line('Following Policies are to be re-worked:');
+        --loop to print the policies list declined by the rater.
+
+        FOR d in rdt LOOP
+                dbms_output.put_line(i||'. '||d.policy);
+                i:=i+1;
+                --query to count the ratingaction policies that are re-rated.
+                select count(ra.coverage) into rcheck from ratingaction ra where ra.action='A' and ra.coverage in 
+                (select coverage from covers cv where cv.policy=d.policy);
+                if(rcheck>0) then
+                        rerated:=rerated+1;
+                end if;       
+        END LOOP;
+
+        --loop to print the policies list declined by the underwriter.
+        FOR k in ct LOOP
+                dbms_output.put_line(i||'. '||k.policy);
+                i:=i+1;
+                 --query to count the underwriting policies that are re-underwrited.
+                select count(policy) into ucheck from underwritingaction uw where uw.action='A' and uw.policy=k.policy;
+                if(ucheck>0) then
+                        reunderwrited:=reunderwrited+1;
+                end if;
+        END LOOP;
+        dbms_output.put_line('Total policies that are  re-rated : '||rerated);
+        dbms_output.put_line('Total policies that are re-underwrited : '||reunderwrited);
+END IF;  
+end;
+/
+
+
+
